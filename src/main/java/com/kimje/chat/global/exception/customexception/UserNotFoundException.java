2bc9a26b3e9e63a51827b4a7256256c72b6800ea
kimjinhoo <wlsgnwkd22@naver.com>
2025-06-12 22:11:04 +0900

    redis설정

--- src/main/java/com/kimje/chat/global/exception/customexception/UserNotFoundException.java
+++ src/main/java/com/kimje/chat/global/exception/customexception/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.kimje.chat.global.exception.customexception;
 
 public class UserNotFoundException extends RuntimeException {
-	public UserNotFoundException(String message , Long userId) {
+	public 	UserNotFoundException(String message , Long userId) {
 		super(message);
 	}
 }
