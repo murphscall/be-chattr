064b715d83c51b2205c482b070c728cff5c0b793
kimjinhoo <wlsgnwkd22@naver.com>
2025-06-18 14:10:37 +0900

    chatUserService 단위테스트

--- src/test/java/com/kimje/chat/chats/service/ChatUserServiceTest.java
+++ src/test/java/com/kimje/chat/chats/service/ChatUserServiceTest.java
@@ -3,20 +3,19 @@ package com.kimje.chat.chats.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
 import java.util.Optional;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import com.kimje.chat.chats.entity.Chat;
 import com.kimje.chat.chats.entity.ChatUser;
+import com.kimje.chat.chats.enums.ChatRole;
 import com.kimje.chat.chats.repository.ChatRoomRepository;
 import com.kimje.chat.chats.repository.ChatUserRepository;
+import com.kimje.chat.user.entity.User;
 import com.kimje.chat.user.repository.UserRepository;
 
 import jakarta.persistence.EntityManager;
@@ -39,14 +38,15 @@ class ChatUserServiceTest {
 	@Mock
 	EntityManager em;
 
+	private static final Long chatId = 1L;
+	private static final Long userId = 1L;
+
 	@Test
 	@DisplayName("채팅방 신규 입장 시 성공")
 	void joinUser() {
-		Long chatId = 1L;
-		Long userId = 1L;
 		int memberCount = 97;
 
-		when(chatRoomRepository.findById(userId)).thenReturn(Optional.of(new Chat()));
+		when(chatRoomRepository.findById(chatId)).thenReturn(Optional.of(new Chat()));
 		when(chatUserRepository.existsByChatIdAndUserId(chatId,userId)).thenReturn(false);
 		when(chatUserRepository.countByChatId(chatId)).thenReturn(memberCount);
 
@@ -58,10 +58,8 @@ class ChatUserServiceTest {
 	@Test
 	@DisplayName("기존 참여자 채팅방 입장 시")
 	void againJoinUser() {
-		Long chatId = 1L;
-		Long userId = 1L;
 
-		when(chatRoomRepository.findById(userId)).thenReturn(Optional.of(new Chat()));
+		when(chatRoomRepository.findById(chatId)).thenReturn(Optional.of(new Chat()));
 		when(chatUserRepository.existsByChatIdAndUserId(chatId,userId)).thenReturn(true);
 
 
@@ -71,11 +69,9 @@ class ChatUserServiceTest {
 	@Test
 	@DisplayName("채팅방 입장 요청 시 인원 초과 예외")
 	public void max_participants(){
-		Long chatId = 1L;
-		Long userId = 1L;
 		int memberCount = 100;
 
-		when(chatRoomRepository.findById(userId)).thenReturn(Optional.of(new Chat()));
+		when(chatRoomRepository.findById(chatId)).thenReturn(Optional.of(new Chat()));
 		when(chatUserRepository.existsByChatIdAndUserId(chatId,userId)).thenReturn(false);
 		when(chatUserRepository.countByChatId(chatId)).thenReturn(memberCount);
 
@@ -86,14 +82,115 @@ class ChatUserServiceTest {
 
 
 	@Test
-	void exitUser() {
+	@DisplayName("채팅방에 참여 하지 않은 유저 퇴장 시도 시 예외 발생")
+	void exitUser_UserNotInChat_ThrowsException() {
+			// given
+			when(chatUserRepository.findByChatIdAndUserId(chatId, userId))
+				.thenReturn(Optional.empty());
+
+			// when & then
+			assertThatThrownBy(() -> chatUserService.exitUser(chatId, userId))
+				.isInstanceOf(IllegalStateException.class)
+				.hasMessage("채팅방에 참여하고 있지 않습니다.");
 	}
 
 	@Test
-	void getMembers() {
+	@DisplayName("일반 유저 퇴장 - 정상 삭제")
+	public void exitUser_RegularMember_DeletesSuccessfully(){
+		ChatUser chatUser = createChatUser(userId,ChatRole.MEMBER);
+		when(chatUserRepository.findByChatIdAndUserId(chatId,userId))
+			.thenReturn(Optional.of(chatUser));
+
+		chatUserService.exitUser(chatId,userId);
+
+		verify(chatUserRepository).delete(chatUser);
+		verify(chatUserRepository, never()).countByChatId(anyLong());
+		verify(chatRoomRepository, never()).deleteById(anyLong());
 	}
 
 	@Test
-	void getCreateByMeChats() {
+	@DisplayName("방장 유저 퇴장 - 혼자 남은 경우")
+	public void exitUser_MasterMember_DeleteSuccessfully(){
+		ChatUser chatUser = createChatUser(userId,ChatRole.MASTER);
+		when(chatUserRepository.findByChatIdAndUserId(chatId,userId))
+			.thenReturn(Optional.of(chatUser));
+		when(chatUserRepository.countByChatId(chatId)).thenReturn(1);
+
+		chatUserService.exitUser(chatId,userId);
+		verify(chatUserRepository).delete(chatUser);
+		verify(chatRoomRepository).deleteById(chatId);
+	}
+
+	@Test
+	@DisplayName("방장 유저 퇴장(매니저 있는 경우) - 매니저에게 권한 양도")
+	public void exitUser_MasterMember_TransferToManager(){
+		ChatUser masterUser = createChatUser(userId,ChatRole.MASTER);
+		ChatUser managerUser = createChatUser(2L, ChatRole.MANAGER);
+
+		when(chatUserRepository.findByChatIdAndUserId(chatId,userId))
+			.thenReturn(Optional.of(masterUser));
+		when(chatUserRepository.countByChatId(chatId)).thenReturn(3);
+		when(chatUserRepository.findFirstByChatIdAndRole(chatId,ChatRole.MANAGER)).thenReturn(Optional.of(managerUser));
+
+		chatUserService.exitUser(chatId,userId);
+
+		assertThat(managerUser.getRole()).isEqualTo(ChatRole.MASTER);
+		verify(chatUserRepository).save(managerUser);
+		verify(chatUserRepository).delete(masterUser);
+
+	}
+
+	@Test
+	@DisplayName("방장 유저 퇴장(매니저 없는 경우) - 유저에게 권한 양도")
+	public void exitUser_MasterMember_TransferToMember(){
+		ChatUser masterUser = createChatUser(userId,ChatRole.MASTER);
+		ChatUser memberUser = createChatUser(3L, ChatRole.MEMBER);
+
+		when(chatUserRepository.findByChatIdAndUserId(chatId,userId))
+			.thenReturn(Optional.of(masterUser));
+		when(chatUserRepository.countByChatId(chatId)).thenReturn(3);
+		when(chatUserRepository.findFirstByChatIdAndRole(chatId,ChatRole.MANAGER)).thenReturn(Optional.empty());
+		when(chatUserRepository.findFirstByChatIdAndRole(chatId,ChatRole.MEMBER)).thenReturn(Optional.of(memberUser));
+
+
+		chatUserService.exitUser(chatId,userId);
+
+
+		assertThat(memberUser.getRole()).isEqualTo(ChatRole.MASTER);
+
+		verify(chatUserRepository).save(memberUser);
+		verify(chatUserRepository).delete(masterUser);
+	}
+
+	@Test
+	@DisplayName("권한 양도할 매니저도 멤버도 없다면 예외 발생")
+	public void failed_transfer_to_member(){
+		ChatUser masterUser = createChatUser(userId,ChatRole.MASTER);
+
+		when(chatUserRepository.findByChatIdAndUserId(chatId,userId))
+			.thenReturn(Optional.of(masterUser));
+		when(chatUserRepository.countByChatId(chatId)).thenReturn(3);
+		when(chatUserRepository.findFirstByChatIdAndRole(chatId,ChatRole.MANAGER)).thenReturn(Optional.empty());
+		when(chatUserRepository.findFirstByChatIdAndRole(chatId,ChatRole.MEMBER)).thenReturn(Optional.empty());
+
+
+		assertThatThrownBy(() ->
+			chatUserService.exitUser(chatId,userId))
+			.isInstanceOf(IllegalStateException.class)
+				.hasMessage("위임할 대상이 없습니다.");
+	}
+
+	// 테스트 헬퍼 메서드
+	private ChatUser createChatUser(Long userId,ChatRole role) {
+		Chat chat = new Chat();
+		chat.setId(chatId);
+		User user = new User();
+		user.setId(userId);
+
+		ChatUser chatUser = new ChatUser();
+		chatUser.setChat(chat);
+		chatUser.setUser(user);
+		chatUser.setRole(role);
+		return chatUser;
 	}
 }
