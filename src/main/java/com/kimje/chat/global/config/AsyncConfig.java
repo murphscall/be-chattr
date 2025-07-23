04d5e9cacb3967c93c1d3221102dabeb3223f175
kimjinhoo <wlsgnwkd22@naver.com>
2025-07-23 13:31:42 +0900

    refactor(AsyncConfig 수정 및 Async 예외 핸들러 추가): 기본적인 SimpleAsyncTaskExecutor 사용시 매번 새로운 스레드를 생성하는 문제 처리 / 예외 핸들러 추가
    SimpleAsyncTaskExecutor 사용 시 매번 새로운 스레드를 생성하면 리소스 낭비 및 성능 저하 , 스케일링 문제
    - CustomAsyncExecutor 사용하여 제한된 리소스로 사용
    
    비동기 처리 중 예외 발생 시 핸들링을 해줄 수 있는 클래스 추가
    - CustomAsyncExceptionHandler  =>  실패 로그 남김

--- src/main/java/com/kimje/chat/global/config/AsyncConfig.java
+++ src/main/java/com/kimje/chat/global/config/AsyncConfig.java
@@ -1,16 +1,19 @@
 package com.kimje.chat.global.config;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import com.kimje.chat.global.exception.exhandler.global.CustomAsyncExceptionHandler;
+
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
 
 	@Bean
 	public Executor customAsyncExecutor(){
@@ -20,5 +23,10 @@ public class AsyncConfig {
 		executor.setThreadNamePrefix("email-thread");
 		executor.initialize();
 		return executor;
+	}
+
+	@Override
+	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+		return new CustomAsyncExceptionHandler();
 	}
 }
